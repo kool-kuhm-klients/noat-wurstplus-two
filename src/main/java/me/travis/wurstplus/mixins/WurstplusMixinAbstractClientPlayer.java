696d3f64f29b446003b022367747c9c8bb550f31
Aut <noatprotonvpn@gmail.com>
2021-06-18 14:36:46 +0700

    add: clockworkmc cape

--- src/main/java/me/travis/wurstplus/mixins/WurstplusMixinAbstractClientPlayer.java
+++ src/main/java/me/travis/wurstplus/mixins/WurstplusMixinAbstractClientPlayer.java
@@ -32,6 +32,9 @@ public abstract class WurstplusMixinAbstractClientPlayer {
             if (Wurstplus.get_setting_manager().get_setting_with_tag("Capes", "CapeCape").in("OG")) {
                 r = new ResourceLocation("custom/cape-old.png");
             }
+            if (Wurstplus.get_setting_manager().get_setting_with_tag("Capes", "CapeCape").in("Clockwork MC")) {
+                r = new ResourceLocation("custom/clockworkmc.png");
+            }
             if (Wurstplus.get_setting_manager().get_setting_with_tag("Capes", "CapeCape").in("Clockwork")) {
               r = new ResourceLocation("custom/clockwork.png");
             } else {
