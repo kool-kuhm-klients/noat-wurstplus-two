c61d877a610c22a0cec56cfe35a9ae85e82bddcd
Aut <noatprotonvpn@gmail.com>
2021-06-22 07:56:12 +0700

    fix friend stuff

--- src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusFriendList.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusFriendList.java
@@ -7,7 +7,7 @@ import me.travis.wurstplus.wurstplustwo.util.WurstplusOnlineFriends;
 import net.minecraft.entity.Entity;
 
 public class WurstplusFriendList extends WurstplusPinnable {
-    
+
     public WurstplusFriendList() {
         super("Friends", "Friends", 1, 0, 0);
     }
@@ -23,12 +23,12 @@ public class WurstplusFriendList extends WurstplusPinnable {
         int nl_b = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorB").get_value(1);
         int nl_a = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorA").get_value(1);
 
-        String line1 = bold + "the_fellas: ";
-        
+        String line1 = bold + "block game goons: ";
+
         passes = 0;
 
         create_line(line1, this.docking(1, line1), 2, nl_r, nl_g, nl_b, nl_a);
-        
+
         if (!WurstplusOnlineFriends.getFriends().isEmpty()) {
             for (Entity e : WurstplusOnlineFriends.getFriends()) {
                 passes++;
@@ -37,7 +37,7 @@ public class WurstplusFriendList extends WurstplusPinnable {
         }
 
 		this.set_width(this.get(line1, "width") + 2);
-		this.set_height(this.get(line1, "height") + 2);
+		this.set_height(this.get(line1, "height") + 5);
 	}
 
 }
